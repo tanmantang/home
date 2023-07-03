d33795a0913dc65f5c4e7ebcf4bf781d0fb21d64
tanmantang <tanmantang@foxmail.com>
2023-07-03 17:17:20 +0800

    优化bug

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -1,45 +1,42 @@
-function hitokoto()
-{
-    $.ajax({
-        url: "https://v1.hitokoto.cn",
-        dataType: "json",
-        async: false,
-        success: function (data) {
-            $("p").append(data + "<br />");
-            if (data.from_who == null) {
-                data.from_who = "佚名";
-            }
-            $('#text').text(data.hitokoto);
-            $('#author').text("— " + data.from + "【" + data.from_who + "】");
-            $("#text").show().typewriter(50);
-            console.log(data.uuid);
-        }
-    })
-}
-
-// 实现打字机效果
-(function ($) {
-    $.fn.typewriter = function () {
-        this.each(function () {
-            var $ele = $(this),
-                str = $ele.html(),
-                progress = 0;
-            $ele.html('');
-            var timer = setInterval(function () {
-                var current = str.substr(progress, 1);
-                if (current == '<') {
-                    progress = str.indexOf('>', progress) + 1;
-                } else {
-                    progress++;
-                }
-                $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-                if (progress >= str.length) {
-                    clearInterval(timer);
-                }
-            }, 150);
+function Hitokoto() {
+    // fetch("https://v1.hitokoto.cn?encode=json")
+    fetch("https://api.vvhan.com/api/love?type=json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            $('#text').text(data.ishan);
+            $("#text").show().typewriter();
+            // var author = !!data.from ? data.from : "无名氏";
+            // $('#author').text("—— " + (data.from_who || '') + "「" + author + "」");
+            $('#author').text("TO 「 梅 」");
+            window.setTimeout(Hitokoto, 10000);
+        })
+        .catch(function (err) {
+            console.error(`获取一言报错，错误信息: ${err.message}. 当前时间: ${new Date().toISOString()}`);
+            // Hitokoto();
         });
-        return this;
-    };
-})(jQuery);
-hitokoto();
-setInterval("hitokoto()",10000);
+}
+var isID = 0;
+if (!isID) { window.setTimeout(Hitokoto, 500);}
+console.info("别抄了，还有BUG，emmmmm，改不完，根本改不完");
+$.fn.typewriter = function () {
+    this.each(function () {
+        var $ele = $(this),str = $ele.html(),progress = 0;
+        $ele.html('');
+        var timer = setInterval(function () {
+            var current = str.substr(progress, 1);
+            if (current == '<') {
+                progress = str.indexOf('>', progress) + 1;
+            } else {
+                progress++;
+            }
+            $ele.html(str.substring(0, progress) + (progress & 1 ? '|' : ''));
+            if (progress >= str.length) {
+                clearInterval(timer);
+            }
+        }, 180);
+    });
+    return this;
+};
+
--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@
 		window.open("mailto:tanmantang@foxmail.com");
 	})
 
+	$(function(){
+		var mobile_flag = isMobile(); // true为PC端，false为手机端
+		if(mobile_flag){
+			alert("手机观感不佳，可以使用PC或者平板哟>_<");
+		}
+	});
+
 	function SVGMenu(el) {
 		this.el = el;
 		this.init();
@@ -89,3 +96,25 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function isMobile() {
+	var userAgentInfo = navigator.userAgent;
+	var mobileAgents = [ "Android", "iPhone", "SymbianOS", "Windows Phone", "iPad","iPod"];
+	var mobile_flag = false;
+	
+	//根据userAgent判断是否是手机
+	for (var v = 0; v < mobileAgents.length; v++) {
+		if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+			mobile_flag = true;
+			break;
+		}
+	}
+	 var screen_width = window.screen.width;
+	 var screen_height = window.screen.height;    
+	 
+	 //根据屏幕分辨率判断是否是手机
+	 if(screen_width < 500 && screen_height < 800){
+		 mobile_flag = true;
+	 }
+	 return mobile_flag;
+}
